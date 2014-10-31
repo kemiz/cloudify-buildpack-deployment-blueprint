d09a9c6d88707117b8c405ad852b648d7daa6e57
Christos Erotocritou <kemimeister@gmail.com>
2014-10-31 10:57:40 +0000

    Fixed stop app

--- scripts/stop-app.sh
+++ scripts/stop-app.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e
 
-PID_FILE="/tmp/nodejs.pid"
-
-PID=`cat ${PID_FILE}`
-ctx logger info "Shutting down nodejs. pid = ${PID}"
-kill -9 ${PID} || exit $?
-
-ctx logger info "Stopped nodecellar application :-)"
-ctx logger info "Removing PID file from ${PID_FILE}"
-rm ${PID_FILE}
+ctx logger info "Stopping application..." 
+sudo docker stop -p kemiz/$APP_NAME
+ctx logger info "Success!" 
